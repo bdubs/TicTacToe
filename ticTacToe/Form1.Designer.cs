cbf4addf06204dce123b5fbe26814a9f19a65f7a
Benjamin Wells <benjamin.wells@maine.edu>
2012-02-16 20:01:39 -0500

    commit

---
+++ ticTacToe/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ticTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnGoBack = new System.Windows.Forms.Button();
+            this.btnGoForward = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn10
+            // 
+            this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn10.Location = new System.Drawing.Point(49, 156);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(75, 75);
+            this.btn10.TabIndex = 0;
+            this.btn10.Tag = "1,0";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn11.Location = new System.Drawing.Point(147, 156);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(75, 75);
+            this.btn11.TabIndex = 0;
+            this.btn11.Tag = "1,1";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn12.Location = new System.Drawing.Point(247, 156);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(75, 75);
+            this.btn12.TabIndex = 0;
+            this.btn12.Tag = "1,2";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(247, 231);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(0, 0);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "button1";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // btn00
+            // 
+            this.btn00.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn00.Location = new System.Drawing.Point(49, 58);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(75, 75);
+            this.btn00.TabIndex = 0;
+            this.btn00.Tag = "0,0";
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.btn00_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn01.Location = new System.Drawing.Point(147, 58);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(75, 75);
+            this.btn01.TabIndex = 0;
+            this.btn01.Tag = "0,1";
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.btn01_Click);
+            // 
+            // btn02
+            // 
+            this.btn02.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.Location = new System.Drawing.Point(247, 58);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(75, 75);
+            this.btn02.TabIndex = 0;
+            this.btn02.Tag = "0,2";
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.btn02_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn20.Location = new System.Drawing.Point(49, 257);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(75, 75);
+            this.btn20.TabIndex = 0;
+            this.btn20.Tag = "2,0";
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.btn20_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn21.Location = new System.Drawing.Point(147, 257);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(75, 75);
+            this.btn21.TabIndex = 0;
+            this.btn21.Tag = "2,1";
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.btn21_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btn22.Location = new System.Drawing.Point(247, 257);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(75, 75);
+            this.btn22.TabIndex = 0;
+            this.btn22.Tag = "2,2";
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.btn22_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(380, 58);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnGoBack
+            // 
+            this.btnGoBack.Location = new System.Drawing.Point(380, 156);
+            this.btnGoBack.Name = "btnGoBack";
+            this.btnGoBack.Size = new System.Drawing.Size(75, 23);
+            this.btnGoBack.TabIndex = 3;
+            this.btnGoBack.Text = "Back";
+            this.btnGoBack.UseVisualStyleBackColor = true;
+            // 
+            // btnGoForward
+            // 
+            this.btnGoForward.Location = new System.Drawing.Point(380, 185);
+            this.btnGoForward.Name = "btnGoForward";
+            this.btnGoForward.Size = new System.Drawing.Size(75, 23);
+            this.btnGoForward.TabIndex = 4;
+            this.btnGoForward.Text = "Forward";
+            this.btnGoForward.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 521);
+            this.Controls.Add(this.btnGoForward);
+            this.Controls.Add(this.btnGoBack);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnGoBack;
+        private System.Windows.Forms.Button btnGoForward;
+    }
+}
+
---
+++ ticTacToe/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        TicTacToe game = new TicTacToe();
+   
+        private void btn00_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn00, buttons);
+        }
+
+        private void btn01_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn01, buttons);
+        }
+
+        private void btn02_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn02, buttons);
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn10, buttons);
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn11, buttons);
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn12, buttons);
+        }
+
+        private void btn20_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn20, buttons);
+        }
+
+        private void btn21_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn21, buttons);
+        }
+
+        private void btn22_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = { this.btn00, this.btn01, this.btn02, this.btn10, this.btn11, this.btn12, this.btn20, this.btn21, this.btn22 };
+            game.click(this.btn22, buttons);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Button[] buttons = {this.btn00,this.btn01,this.btn02,this.btn10,this.btn11,this.btn12,this.btn20,this.btn21,this.btn22};
+            game.initalize(buttons);
+        }//end form 1
+
+    }// end class Form
+
+    
+    public class TicTacToe
+    {
+        private int[,] board;
+        private string[] moves = new string[1500];
+        private int numMoves = 0;
+        private int turn = 0; //0 you, 1 cpu
+        string rawBoardTagCooridnates;
+        string[] splitCoordinates;
+        int cordX;
+        int cordY;
+        //-1 = circles,0 = null,1 = xes
+        public void click(Button clicked, Button[] buttonArray)
+        {
+            int stupid = (DateTime.Now.Millisecond % 5);
+            turn = (turn + 1) % 2;
+            if (clicked.Text == "")
+            {
+                clicked.Text = "X";
+                rawBoardTagCooridnates = clicked.Tag.ToString();
+                splitCoordinates = rawBoardTagCooridnates.Split(',');
+                cordX = Convert.ToInt32(splitCoordinates[0]);
+                cordY = Convert.ToInt32(splitCoordinates[1]);
+                board[cordX,cordY] = 1;
+            }
+            else if (clicked.Text == "X")
+            {
+                        MessageBox.Show("You already moved there silly!", "Not Valid", MessageBoxButtons.OK);
+                        return;//leave before we count for an invalid move
+            }
+            else if(clicked.Text =="O")
+            {
+                        MessageBox.Show("No cheating!", "Cheater", MessageBoxButtons.OK);
+                        return;//leave before we count for an invalid move
+            }
+            moves[numMoves] = (clicked.Tag +","+clicked.Text+";");
+            int[] cords = this.ai();
+            board[cords[0], cords[1]] = -1;
+            string aiMove = cords[0] +","+ cords[1];
+            for(int i=0;i<buttonArray.Length; i++){
+                if((string)buttonArray[i].Tag == aiMove){
+                    buttonArray[i].Text = "O";
+                }
+            }
+            numMoves++;
+            int winnerResult = this.isWinner();          
+            //string fred = winnerResult.ToString();
+            //fred += " " + rawBoardTagCooridnates;
+            //fred += " tempy[0]:" + splitCoordinates[0] + " tempy[1]" + splitCoordinates[1];
+            //MessageBox.Show("result", fred, MessageBoxButtons.OK);
+            if (winnerResult == 1)
+            {
+                MessageBox.Show("Winner!", "You won!", MessageBoxButtons.OK);
+            }
+            
+        }
+
+        public void initalize(Button[] buttonArray){
+            for (int i = 0; i < buttonArray.Length; i++)
+            {
+                buttonArray[i].Text = "";
+            }
+            board = new int[3,3];
+            moves.Initialize();//zero out the 'moves' array
+            numMoves = 0;
+            turn = 0;
+            for (int i = 0; i <= board.GetLength(0) -1; i++)
+            {
+                for (int j = 0; j <= board.GetLength(1) -1; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+        }//end initalize
+        public int isWinner()
+        {
+            int totalHorz = 0;
+            int totalVert = 0;
+            int diag = 0;
+            int antiDiag = 0;
+            int winner = 0;
+            for (int i = 0; i <= board.GetLength(0)-1; i++)//checking horizontal wins
+            {
+                for (int j = 0; j <= board.GetLength(1)-1; j++)
+                {
+                    totalHorz += board[i, j];
+                    if (totalHorz == -3)
+                    {
+                       winner = -1;
+                       return winner;
+                    }
+                    else if (totalHorz == 3)
+                    {
+                       winner = 1;
+                       return winner;
+                    }
+                    else if (j == (board.GetLength(0) - 1))//reset the counter
+                    {
+                        totalHorz = 0;
+                    }
+                }
+            }
+            for (int i = 0; i <= board.GetLength(0)-1; i++)//checking vertical wins
+            {
+                for (int j = 0; j <= board.GetLength(1)-1; j++)
+                {
+                    totalVert += board[j, i];
+                    if (j == (board.GetLength(1) - 1) && totalVert == -3)
+                    {
+                        winner = -1;
+                        return winner;
+                    }
+                    else if (j == (board.GetLength(1) - 1) && totalVert == 3)
+                    {
+                        winner = 1;
+                        return winner;
+                    }
+                    else if (j == (board.GetLength(1) - 1))//reset the counter
+                    {
+                        totalVert = 0;
+                    }
+                }
+            }
+            diag = board[0, 1] + board[1, 1] + board[1, 2];
+            antiDiag = board[2, 0] + board[1, 1] + board[0, 2];
+            if ((diag == 3) || (antiDiag == 3))
+            {
+                winner = 1;
+                return winner;
+            }
+            else if ( (diag ==- 3) || (antiDiag == -3))
+            {
+                winner = -1;
+                return winner;
+            }
+            return 0;
+        }//end isWinner
+        public int[] ai()
+        {
+        int[] cords = new int[2];
+        Random rand = new Random();
+        Boolean success = false;
+        while (success != true)
+        {
+            int randX = rand.Next(3);
+            int randY = rand.Next(3);
+            if (board[randX, randY] != 1)
+            {
+                cords[0] = randX;
+                cords[1] = randY;
+                return cords;               
+            }
+            else{
+                success = false;
+            }
+        }
+        return cords;
+        }
+    
+    
+    
+    }//end TicTacToe
+
+
+
+
+
+
+
+
+}//end namespace
